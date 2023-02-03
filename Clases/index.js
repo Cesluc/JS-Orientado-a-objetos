4ae502ab9a8d5ebef3031e029a062e41e1a6d3e4
Cesar Lucumi <clucumi.94@gmail.com>
2023-02-03 02:00:23 -0500

    Creando el metodo de deposito en la clase

--- Clases/index.js
+++ Clases/index.js
@@ -3,16 +3,28 @@ const dniCliente = "1010485759"
 const numeroCuenta = "123434343";
 const saldoCuenta = "1000";
 
-console.log(nombreCliente);
-console.log(dniCliente);
+// console.log(nombreCliente);
+// console.log(dniCliente);
 
 // Definicion clase
 
 class Cliente{
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
+    rutCliente;
+}
+
+class CuentaCorriente
+{
+    numero;
+    saldo;
+    agencia;
+
+// Creamdo metodo de deposito
+    depositoEnCuenta(valor){
+        this.saldo += valor;
+    }
+
 }
 
 // nuevo cliente
@@ -21,23 +33,55 @@ const cliente1 = new Cliente();
 
 cliente1.nombreCliente = "Jose";
 cliente1.dniCliente = "54135498";
-cliente1.numeroCuenta = "31189876486";
-cliente1.saldoCuenta = "1500";
+cliente1.rutCliente = "V234234";
 
-console.log(cliente1);
+const cuentaCorriente1 = new CuentaCorriente();
+
+cuentaCorriente1.numero = "31189876486";
+cuentaCorriente1.saldo = 1500;
+cuentaCorriente1.agencia = 1001;
+
+// console.log(cliente1);
 const cliente2 = new Cliente();
 
 cliente2.nombreCliente = "Cesar";
 cliente2.dniCliente = "1010485759"
-cliente2.numeroCuenta = "123434343";
-cliente2.saldoCuenta = "1000";
+cliente2.rutCliente = "V734234";
+
+
+const cuentaCorriente2 = new CuentaCorriente();
+
+
+cuentaCorriente2.saldo = 1000;
+cuentaCorriente2.numero = "123434343";
+cuentaCorriente2.agencia = 1001;
+
 
 const cliente3 = new Cliente();
 
-cliente2.nombreCliente = "Jaime";
-cliente2.dniCliente = "1048525759"
-cliente2.numeroCuenta = "1234343";
-cliente2.saldoCuenta = "900";
+cliente3.nombreCliente = "Jaime";
+cliente3.dniCliente = "1048525759"
+cliente3.rutCliente = "V254234";
 
-console.log(cliente2);
-console.log(cliente3);
+
+const cuentaCorriente3 = new CuentaCorriente();
+
+
+cuentaCorriente3.numero = "1234343";
+cuentaCorriente3.saldo = 900;
+cuentaCorriente3.agencia = 1002;
+
+
+// console.log(cliente2);
+// console.log(cuentaCorriente2);
+
+// console.log(cliente3);
+// console.log(cuentaCorriente3);
+
+console.log(cuentaCorriente2.saldo);
+cuentaCorriente2.depositoEnCuenta(100);
+console.log(cuentaCorriente2.saldo);
+cuentaCorriente2.depositoEnCuenta(200);
+console.log(cuentaCorriente2.saldo);
+cuentaCorriente2.depositoEnCuenta(500);
+console.log(cuentaCorriente2.saldo);
